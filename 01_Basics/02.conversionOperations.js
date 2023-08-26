dcdde11f1dc5c24bb5b03059de7f07f7c318fe08
Arijit Das <124693190+Arijit9721@users.noreply.github.com>
2023-08-26 18:33:19 +0000

    03 lesson

--- 01_Basics/02.conversionOperations.js
+++ 01_Basics/02.conversionOperations.js
@@ -1,3 +1,7 @@
 let num = null
 let typecast = Number(num);
-console.log(typecast)
+// console.log(typecast)
+console.log(1+2+'3'+4+5);
+console.log('1'+2+3);
+// everything ahead of string is considered number so the operation takes place, after the first
+//  string everything is considered string  
