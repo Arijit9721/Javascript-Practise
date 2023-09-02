9a74fc373b2e975e878870f70e4954aa4dc9ea08
Arijit Das <124693190+Arijit9721@users.noreply.github.com>
2023-09-02 17:06:08 +0000

    scope 2

--- 02_IntBasics/02.Scopes.js
+++ 02_IntBasics/02.Scopes.js
@@ -1,1 +1,12 @@
-hvhkv
+sayHi()
+function sayHi() {  // This method is called function declaration
+    console.log( "Hello" );
+  }
+//   as the function is created by declaration , it can be called even before it is crreated.
+// this is  because of hoisting
+
+console.log(hello);
+// this will not work as expression does not support hoisting
+const hello = function hi(){ // this method is called function expression 
+    console.log('hello')
+}
