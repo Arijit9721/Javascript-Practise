8faf25c4387b1fa53106310b85d2902e5a80864a
Arijit Das <124693190+Arijit9721@users.noreply.github.com>
2023-09-03 19:22:11 +0000

    switch

---
+++ 02_IntBasics/05.switch.js
@@ -0,0 +1,45 @@
+// finding out the name of the month
+
+let month = 10
+switch (month) {
+    case 1:
+        console.log(`the month is : January`);
+        break;
+    case 2:
+        console.log(`the month is : February`);
+        break;
+    case 3:
+        console.log(`the month is : March`);
+        break;
+    case 4:
+        console.log(`the month is : April`);
+        break;
+    case 5:
+        console.log(`the month is : May`);
+        break;
+    case 6:
+        console.log(`the month is : June`);
+        break;
+    case 7:
+        console.log(`the month is : July`);
+        break;
+    case 8:
+        console.log(`the month is : August`);
+        break;
+    case 9:
+        console.log(`the month is : September`);
+        break;
+    case 10:
+        console.log(`the month is : October`);
+        break;
+    case 11:
+        console.log(`the month is : November`);
+        break;
+    case 12:
+        console.log(`the month is : December`);
+        break;
+
+    default:
+        console.log(`Not a valid month`);
+        break;
+}
