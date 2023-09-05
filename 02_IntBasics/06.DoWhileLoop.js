d3e026d5f85d9f9559c6c48733bf5d953276217c
Arijit Das <124693190+Arijit9721@users.noreply.github.com>
2023-09-05 16:44:29 +0000

    scare

---
+++ 02_IntBasics/06.DoWhileLoop.js
@@ -0,0 +1,8 @@
+let score1 = 100
+let aim = 1000
+do {
+    score1+=100
+    console.log(`the current score is : ${score1}`);
+} while (score1<aim);
+// dowhile loop executes the code first and then checks the condition 
+// it is rarely used
---
+++ 02_IntBasics/07.highOrderArrayLoops.js
@@ -0,0 +1,15 @@
+const testString = 'arijit'
+const testArray = [1,2,3,4,5]
+const testObject = {
+    num: 1,
+    case: 'test'
+}
+
+// For of loop (iterates till the end, similar to --> for i:array in c++)
+for (const i of testArray) {
+    console.log(i);
+}
+for (const i of testString) {
+    console.log(i);
+}
+    
